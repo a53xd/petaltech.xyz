30624daa489c703c61534a48baf021a0f1918c58
ashton! <122162553+im-ashton@users.noreply.github.com>
2024-05-11 13:16:33 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
         </a>
 
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://dsc.gg/bloomlabs"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -95,7 +95,7 @@ export default function Home() {
         </a>
 
         <a
-          href="https://dsc.gg/bloomlabs"
+          href="https://Ko-fi.com/bloomexp"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
